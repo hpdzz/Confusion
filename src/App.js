117d67da11d1b46f24e67828f26140b232fa8882
hpdzz <tranlehuuphuoc22502@gmail.com>
2023-09-26 16:20:37 +0700

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import { Component } from 'react';
+import React, { Component } from 'react';
+import Menu from './components/menuComponents';
+import { DISHES } from './shared/dishes';
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
   render() {
     return (
       <div className="App">
@@ -12,11 +20,10 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
 }
-
-
 
 export default App;
